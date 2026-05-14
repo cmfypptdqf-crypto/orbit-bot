8efa3ab0a16b39713cb7c21c08c7aad6bf1d5add
7sevenK <cmfypptdqf@privaterelay.appleid.com>
2026-05-13 21:48:46 -0300

    Atualizar o rankglobal.js

--- commands/economia/rankglobal.js
+++ commands/economia/rankglobal.js
@@ -1,4 +1,4 @@
-const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
 
@@ -6,7 +6,7 @@ const dbPath = path.join(__dirname, '..', '..', 'database.json');
 
 function getDB() {
     if (!fs.existsSync(dbPath)) {
-        fs.writeFileSync(dbPath, JSON.stringify({ usuarios: {} }));
+        fs.writeFileSync(dbPath, JSON.stringify({ usuarios: {}, vip_list: {} }));
     }
     return JSON.parse(fs.readFileSync(dbPath, 'utf8'));
 }
@@ -31,11 +31,15 @@ module.exports = {
                 }
                 
                 if (user) {
+                    const isVip = db.vip_list[userId] && db.vip_list[userId].expira > Date.now();
+                    const vipTier = isVip ? db.vip_list[userId].tier : null;
+                    
                     ranking.push({
                         user: user,
                         total: total,
-                        carteira: data.carteira || 0,
-                        banco: data.banco || 0
+                        vip: isVip,
+                        vipTier: vipTier,
+                        missoes: data.total_missoes || 0
                     });
                 }
             }
@@ -45,17 +49,14 @@ module.exports = {
         const top10 = ranking.slice(0, 10);
         
         if (top10.length === 0) {
-            return message.reply('📊 Nenhum usuário tem moedas ainda!');
+            return message.reply('📊 Nenhum usuário tem Orbs ainda!');
         }
         
         const embed = new EmbedBuilder()
             .setColor(0xFFD700)
-            .setTitle('🏆 Ranking Global de Riqueza')
-            .setDescription('Os usuários mais ricos em TODOS os servidores!')
-            .addFields(
-                { name: '📊 Total de usuários', value: `${ranking.length}`, inline: true }
-            )
-            .setTimestamp();
+            .setTitle('🏆 Ranking Galáctico de Riqueza')
+            .setDescription('Os exploradores mais ricos do universo!')
+            .setFooter({ text: '⭐ = VIP' });
         
         for (let i = 0; i < top10.length; i++) {
             const pos = i + 1;
@@ -64,19 +65,18 @@ module.exports = {
             else if (pos === 2) medalha = '🥈 ';
             else if (pos === 3) medalha = '🥉 ';
             
+            let vipIcon = '';
+            if (top10[i].vip) {
+                if (top10[i].vipTier === 'diamante') vipIcon = ' 💎';
+                else if (top10[i].vipTier === 'ouro') vipIcon = ' ⭐';
+                else if (top10[i].vipTier === 'prata') vipIcon = ' ✨';
+                else vipIcon = ' 🌟';
+            }
+            
             embed.addFields({
-                name: `${medalha}#${pos} - ${top10[i].user.username}`,
-                value: `💰 ${top10[i].total.toLocaleString()} orbs (💵 ${top10[i].carteira.toLocaleString()} | 🏦 ${top10[i].banco.toLocaleString()})`,
+                name: `${medalha}#${pos} - ${top10[i].user.username}${vipIcon}`,
+                value: `💰 ${top10[i].total.toLocaleString()} Orbs | 🚀 ${top10[i].missoes} missões`,
                 inline: false
-            });
-        }
-        
-        const userRank = ranking.findIndex(r => r.user.id === message.author.id) + 1;
-        const userData = ranking.find(r => r.user.id === message.author.id);
-        
-        if (userRank > 0 && userData) {
-            embed.setFooter({ 
-                text: `Sua posição: #${userRank} de ${ranking.length} usuários | Patrimônio: ${userData.total.toLocaleString()} moedas` 
             });
         }
         
