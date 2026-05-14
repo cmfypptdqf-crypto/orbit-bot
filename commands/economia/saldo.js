37e6f91d25bf65c4d922a7dcb36ffdb72d92715a
7sevenK <cmfypptdqf@privaterelay.appleid.com>
2026-05-14 09:45:49 -0300

    Atualizar o saldo.js

--- commands/economia/saldo.js
+++ commands/economia/saldo.js
@@ -2,7 +2,7 @@
 const { EmbedBuilder } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
-const { getBonusDoUsuario, getVIPBonus } = require('../../utilidades/galaxiaBonus.js');
+const { getBonusDoUsuario, getVIPBonus } = require('../utilidades/galaxiaBonus.js');
 
 const dbPath = path.join(__dirname, '..', '..', 'database.json');
 
