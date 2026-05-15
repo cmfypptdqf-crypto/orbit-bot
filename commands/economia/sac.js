e059c188edb2b014c262b0bb1bc2cb9b881cd250
7sevenK <cmfypptdqf@privaterelay.appleid.com>
2026-05-15 06:19:14 -0300

    Atualizar o sac.js

--- commands/economia/sac.js
+++ commands/economia/sac.js
@@ -51,11 +51,11 @@ module.exports = {
         
         const embed = new EmbedBuilder()
             .setColor(0x00008B)
-            .setTitle(`💸 ${getRandomFrase('sucesso')}`)
-            .setDescription(`📡 Você sacou **${amount.toLocaleString()} Orbs** do **Orbital Bank**!`)
+            .setTitle(`<a:h_checkazul:1503775331163705614> ${getRandomFrase('sucesso')}`)
+            .setDescription(`Você sacou **${amount.toLocaleString()} Orbs** do **Orbital Bank**!`)
             .addFields(
-                { name: '💵 Carteira', value: `${db.usuarios[userId].carteira.toLocaleString()} Orbs`, inline: true },
-                { name: '🏦 Orbital Bank', value: `${db.usuarios[userId].banco.toLocaleString()} Orbs`, inline: true }
+                { name: '<a:gcoin:1503617439202545757> Carteira', value: `${db.usuarios[userId].carteira.toLocaleString()} Orbs`, inline: true },
+                { name: '<a:gcoin:1503617439202545757> Orbital Bank', value: `${db.usuarios[userId].banco.toLocaleString()} Orbs`, inline: true }
             )
             .setFooter({ text: '✨ Orbital Bank • Fundos garantidos pela Federação' });
         
